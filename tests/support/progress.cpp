14504d37920afbae25f167b256000d3d75f64a35
Roman Lipovsky <roman.lipovsky@gmail.com>
2025-03-25 19:48:05 +0300

    tune test

--- tests/support/progress.cpp
+++ tests/support/progress.cpp
@@ -8,7 +8,7 @@ TEST_SUITE(Progress) {
   SIMPLE_TEST(Bar) {
     wheels::ProgressBar progress("Steps");
 
-    static const size_t kSteps = 10'000'000;
+    static const size_t kSteps = 1'000'000;
 
     progress.Start(kSteps);
     for (size_t i = 0; i < kSteps; ++i) {
@@ -21,7 +21,7 @@ TEST_SUITE(Progress) {
     wheels::ProgressBar progress("Steps",
       wheels::ProgressBar::Options{false, '#', 50, false});
 
-    static const size_t kSteps = 10'000'000;
+    static const size_t kSteps = 1'000'000;
 
     progress.Start(kSteps);
     for (size_t i = 0; i < kSteps; ++i) {
